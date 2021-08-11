a4dd7d43f8ba6b22e9a9408cd719a1a662cbd503
Kevin Huang <hkt999@gmail.com>
2021-08-11 10:02:51 +0800

    update tensorflow-lite libraries

---
+++ cplibs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp ~/work/tensorflow/tensorflow/lite/build/clog/libclog.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/cpuinfo/libcpuinfo.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/_deps/fft2d-build/libfft2d_fftsg2d.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/_deps/fft2d-build/libfft2d_fftsg.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/_deps/flatbuffers-build/libflatbuffers.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/pthreadpool/libpthreadpool.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/_deps/ruy-build/libruy.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/libtensorflow-lite.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/_deps/xnnpack-build/libXNNPACK.a ./libs
+cp ~/work/tensorflow/tensorflow/lite/build/_deps/farmhash-build/libfarmhash.a ./libs
